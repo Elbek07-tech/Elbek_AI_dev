3d4051d08b5cc9be714b9445dc2b199a1ca250cf
Elbek_AI_dev <hellounitedstatesofamerica07@gmail.com>
2025-01-11 22:48:24 +0500

    Add files via upload

---
+++ while.master.py
@@ -0,0 +1,184 @@
+#name=input("Enter your name:  ")
+#while name!="":
+#    print(f"Your name is {name}")
+#    name=input("Enter your name:  ")
+#print("Wrong")    
+
+#n=1
+#while n<=8:
+    
+#    print(n,end=" ")
+#    n+=1
+#    if n==5:
+#      
+#     continue
+    
+#print("Bye")    
+    
+#friends=[]
+#n=1
+#ques=input("Enter the name of a frend (e to exit):  ")
+#while True:
+#    ques=input("Enter the name of a frend (e to exit):  ")
+#    if ques=="e":
+#        break
+    
+#    print(f"{n}- friend is {ques}")
+#    friends.append(ques)
+#    n+=1
+#    oo=input("Wanna continue.(yes/no):  ")
+#    if oo=="no":
+#        #ques=input("Enter the name of a frend (e to exit):  ")
+#      print("BYE")    
+#      break 
+#print("See yaa")
+#print("BYE")
+
+#t=int(input("Enter any num(q to quit):  "))
+
+#while t !="q":
+#    print(t,end=" ")
+#    t-=1
+    
+
+#    if t==0:
+#        break
+#print("Time's up")    
+    
+
+
+#money="100$"
+
+#while True:# ques!="quit":
+#    ques=input("What you wanna know(q to quit): ")
+#    if ques=="q":
+#      break
+#    print(f"You wanna know {ques}. It'll be soon revealed.")
+#    if ques=="balance":print(money)
+#    else:print("No info for now")
+#    ques=input("What you wanna know(q to quit): ")
+#print("You are out of programme")
+
+
+#n=1
+#num=int(input("Enter number: "))
+#while num:
+    
+#    print(num*n,end=" ")
+#    n+=1
+ #   if n==10:
+#        break
+#print("< Here's the multiplication till 10.")
+
+#info={}
+#name=input("Name: ")
+#age=int(input("Age: "))
+
+#while True:
+#    info[name]=age
+#    ques=input("Wanna add any(yes/no): ")
+#    if ques!="yes":
+#        break
+#    name=input("Name: ")
+#   age=int(input("Age: "))
+#print("Here is the dic")
+#print(info)
+
+
+#code="sky"
+##try_code=input("Code please:")
+#while True:
+#    try_code=input("Code please:")
+#    if try_code !=code:
+ #       print("try again")
+ #   #try_code=input("Code please:")
+    
+#    else:
+#        print("You're in")
+ #       break
+ #   #try_code=input("Code please:")
+ #   #break
+    
+
+#num=int(input("N= "))
+#m=num
+#summa=[]
+#while num>0:
+#    num-=1    
+#    summa.append(num)
+#    if num==0:
+#        break
+#print(sum(summa),"<<< bu",m,"danshlab 0 gachah yig'indi")    
+
+#while True:
+   
+#    x=int(input("quyi: "))
+#    y=int(input("choqqi: "))
+
+#    lst=list(range(x,y))
+#    lst.sort(reverse=True)  
+#    print(lst)
+#    o=input("wanna do again(yes/no):")
+#    if o!="yes":
+#      break 
+   # else:
+    #    x=int(input("quyi: "))
+    #    y=int(input("choqqi: "))
+#print("THE END")
+        
+ 
+        
+#st=[]
+#while True:
+#    q=int(input("Enter a number: "))
+#    if q%2==0:
+#        qu=input("Wanna add (yes/no):")    
+#        if qu!="yes":
+#            break
+#       continue
+#    
+#   else:
+#         st.append(q)
+#   qu=input("Wanna add (yes/no):")    
+#    if qu!="yes":
+#        break
+#print(st)  
+#print("The end!")
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+
+
+
+
+
+
+
+
+
+
